83b17a9fe1326f1103f2014f08919300e818b6c6
mhrls on mhcsxqweb104 <admin@mhcomm.fr>
2025-02-05 19:25:20 +0100

    server handles ldap.initialize

--- src/ad_prx/srv.py
+++ src/ad_prx/srv.py
@@ -3,17 +3,55 @@ AD PRX Helper to forward ldap requests via https
 """
 import os
 import ldap
+import uuid
+
+from datetime import datetime
+from datetime import timedelta
 
 from sanic import Sanic
 from sanic.request import Request
 from sanic.response import text
+from sanic.response import json
 
 
 app = Sanic("AD_PRX")
 
 
 PFX = os.environ.get("AD_PRX_PREFIX", "") # prefix for web server
-objects = {}  # lsit of persistant objects
+
+
+class Object:
+    objects = {}
+    def __init__(self, value, typ=None):
+        self.id = self._gen_id()
+        self.val = value
+        self.type = typ
+        self.created = datetime.now()
+        self.add_obj()
+        cls = type(self)
+        cls.purge_objects()
+
+    def _add_obj(self):
+        """
+        adds object to object store
+        """
+        cls = type(self)
+        cls.objects[obj.id] = self
+
+    @staticmethod
+    def gen_obj_id():
+        # Generate a random UUID (Universally Unique Identifier)
+        unique_id = uuid.uuid4()
+        return unique_id
+
+    def purge(self, seconds=300):
+        t_keep = datetime.now() - timedelta(seconds=seconds)
+        cls = type(self)
+        objects = cls.objects
+        to_purge = [obj.id for obj in objects.values() if obj.created < t_keep]
+        for id_ in to_purge:
+            del objects[id_]
+
 
 def check_auth(request):
     """ is request authenticated """
@@ -24,3 +62,35 @@ def check_auth(request):
 async def index(request: Request):
     check_auth(request)
     return text(f"{PFX}", status=200)
+
+@app.post(f"{PFX}/initialize")
+async def initialize(request: Request):
+    check_auth(request)
+    data = request.json
+    print(f"{data=}")
+    args = data["args"]
+    kwargs = data["kwargs"]
+    con = ldap.initialize(*args, **kwargs)
+    id_ = gen_obj_id()
+    obj = Object(value=con, typ="connection")
+    
+    rslt = {
+        "id": obj.id,
+        "typ": typ,
+    }
+    return json(rslt)
+    
+
+# To IMPLEMENT
+
+# con = ldap.initialize()
+# meth = ldap.sasl.digest.md5()
+# ctrl = ldap.controls.SimplePagedResultsControl(
+# 
+# # 
+# con.set_option()
+# con.simple_bind_s(uid, passwd)
+# con.sasl_interactive_bind_s("", meth)
+# con.search_ext()
+# con.result3()
+
